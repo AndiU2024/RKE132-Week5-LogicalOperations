c5d5701b6a0be91b6ff32804a7904ae9d8619b2d
Andi <andi.uhle@tktk.ee>
2025-03-09 16:34:14 +0200

    Logical Operations

---
+++ OROperator/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+int math, chemistry;
+
+Console.WriteLine("Enter your Math result");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry result");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if (math >= 90 || chemistry >= 90)
+{
+    Console.WriteLine("Congratz! You have been accepted!");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approven");
+}
